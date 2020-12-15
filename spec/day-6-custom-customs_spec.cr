58476917ba7237731097ae91979a3c2822491004
Mike Jongen <mikejongen@hotmail.com>
2020-12-15 19:37:31 +0100

    Initial commit.

---
+++ spec/day-6-custom-customs_spec.cr
@@ -0,0 +1,9 @@
+require "./spec_helper"
+
+describe Day::6::Custom::Customs do
+  # TODO: Write tests
+
+  it "works" do
+    false.should eq(true)
+  end
+end
---
+++ src/day-6-custom-customs.cr
@@ -0,0 +1,6 @@
+# TODO: Write documentation for `Day::6::Custom::Customs`
+module Day::6::Custom::Customs
+  VERSION = "0.1.0"
+
+  # TODO: Put your code here
+end
